a602dbd2b8f1a7c382b8f39b1c09b575002d61a0
Henri <henridvf@gmail.com>
2021-12-13 16:19:13 +0000

    add bootstrap

--- src/components/DisplayAppointments/index.js
+++ src/components/DisplayAppointments/index.js
@@ -5,27 +5,7 @@ const DisplayAppointments = () => {
 
     return (
         <div>
-            <Container>
-                <h1>Create Appointment</h1>
-                <FormControl fullWidth marginBottom={3}>
-                    <InputLabel id="doctors">Select doctor...</InputLabel>
-                    <Select
-                        labelId="doctors"
-                        id="ddDoctors"
-                        // value={age}
-                        // label="Age"
-                        // onChange={handleChange}
-                    >
-                        <MenuItem value={10}>Dr. Seuss</MenuItem>
-                        <MenuItem value={20}>Dr. Gandhi</MenuItem>
-                        <MenuItem value={30}>Dr. Doolittle</MenuItem>
-                    </Select>
-                </FormControl>
-
-                <div>
-
-                </div>
-            </Container>
+            <h1>Create Appointment</h1>
         </div>
     )
 };
