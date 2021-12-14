7177d7cf6d68cbcf80993f90b14a5120e979be2c
Judy <judyelewisjones@hotmail.co.uk>
2021-12-14 10:37:20 +0000

    created a form page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NewAppointmentForm from "./components/NewAppointmentForm";
 import DisplayAppointments from "./components/DisplayAppointments";
 
 function App() {
     return (
+        <div>
             <DisplayAppointments/>
+            <NewAppointmentForm/>
+        </div>
     );
 }
 
