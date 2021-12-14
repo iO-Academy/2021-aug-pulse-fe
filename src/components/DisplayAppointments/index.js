6515df68e765e6c16ac757048f104e8176f37d84
Henri <henridvf@gmail.com>
2021-12-14 10:09:03 +0000

    add components for DisplayAppointments

--- src/components/DisplayAppointments/index.js
+++ src/components/DisplayAppointments/index.js
@@ -1,12 +1,56 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Button, Container, DropdownButton, Dropdown, Form} from "react-bootstrap";
+import AvailableAppointments from "../AvailableAppointments";
+
+const DUMMY_SLOTS = [
+    {
+        id: '8',
+        slot: '8:00 am'
+    },
+    {
+        id: '9',
+        slot: '9:00 am'
+    },
+    {
+        id: '10',
+        slot: '10:00 am'
+    },
+    {
+        id: '11',
+        slot: '11:00 am'
+    },
+    {
+        id: '12',
+        slot: '12:00 pm'
+    }
+];
 
 const DisplayAppointments = () => {
-    const [value, setValue] = useState();
-
     return (
-        <div>
-            <h1>Create Appointment</h1>
-        </div>
+        <Container>
+            <Form>
+                <h2>Create Appointment</h2>
+                <Form.Group className="mb-3" controlId="form.doctorDropdown">
+                    <Form.Select aria-label="Select Doctor">
+                        <option>Select doctor...</option>
+                        <option value="1">Dr. Seuss</option>
+                        <option value="2">Dr. Gandhi</option>
+                        <option value="3">Dr. Doolittle</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="form.calendar">
+                    <Form.Control type="date"/>
+                </Form.Group>
+                <Button>Show available time slots</Button>
+            </Form>
+            {/*<DropdownButton id="dropdown-basic-button" title="Dropdown button">*/}
+            {/*    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>*/}
+            {/*    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>*/}
+            {/*    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
+            {/*</DropdownButton>*/}
+            <AvailableAppointments appointments={DUMMY_SLOTS}/>
+            <Button className="mt-2">Continue</Button>
+        </Container>
     )
 };
 
